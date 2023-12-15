78c8d362eebd48974766ede95c850bdecd04834a
snydernb1 <snydernb1@gmail.com>
2023-12-15 13:23:55 -0700

    Added search feature

--- src/static/app.js
+++ src/static/app.js
@@ -1,13 +1,14 @@
 window.addEventListener("DOMContentLoaded", setup);
 
 
+function displayProducts(products) {
 
-async function setup() {
-	// START HERE
-	// PRODUCTS CAN BE FETCHED USING: GET /products
-	const res = await fetch('http://localhost:3000/products')
-	const products = await res.json()
+	if (document.getElementById('products')) {
+		document.getElementById('products').remove()
+	}
 
+	let productDiv = document.createElement('div')
+	productDiv.setAttribute('id', 'products')
 
 	products.forEach(ele => {
 		let outerDiv = document.createElement('div')
@@ -32,8 +33,53 @@ async function setup() {
 		textDiv.appendChild(price)
 		outerDiv.appendChild(textDiv)
 
-		document.getElementById('products').appendChild(outerDiv)
+		productDiv.appendChild(outerDiv)
 	});
+
+	document.getElementById('main').appendChild(productDiv)
+}
+
+
+async function setup() {
+	// START HERE
+	// PRODUCTS CAN BE FETCHED USING: GET /products
+	const res = await fetch('http://localhost:3000/products')
+	const products = await res.json()
+
+	displayProducts(products)
+
+
+
+	const input = document.querySelector('input')
+	input.addEventListener('input', searchListings)
+
+	function searchListings(e) {
+
+			const searchedProducts = []
+			let searchText = e.target.value.toLowerCase()
+
+			products.forEach(ele => {
+				if (ele.title.toLowerCase().includes(searchText)) {
+					searchedProducts.push(ele)
+				}
+			})
+
+			displayProducts(searchedProducts)
+
+
+	}
+
+	const homeButton = document.getElementById('homeButton')
+	homeButton.addEventListener('click', returnHome)
+
+	function returnHome (e) {
+		document.querySelector('input').value = ''
+		displayProducts(products)
+
+	}
+
+	return true
+
 }
 
 
