fbed902adbfa4b51556877e975dd85a663c3a633
snydernb1 <snydernb1@gmail.com>
2023-12-15 10:49:14 -0700

    basic JS script written to grab listings, basic css and html added

--- src/static/app.js
+++ src/static/app.js
@@ -1,6 +1,36 @@
 window.addEventListener("DOMContentLoaded", setup);
 
+
+
 async function setup() {
 	// START HERE
 	// PRODUCTS CAN BE FETCHED USING: GET /products
+	const res = await fetch('http://localhost:3000/products')
+	const products = await res.json()
+
+
+	products.forEach(ele => {
+		let outerDiv = document.createElement('div')
+		let photo = document.createElement('img')
+		let title = document.createElement('h4')
+		let price = document.createElement('p')
+
+		photo.src = ele.images[0].src
+		photo.setAttribute('class', 'imgs')
+
+		title.textContent = `${ele.title}`
+		title.setAttribute('class', 'title')
+
+		price.textContent = `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(ele.price/100)}`
+		price.setAttribute('class', 'price')
+
+		outerDiv.appendChild(photo)
+		outerDiv.appendChild(title)
+		outerDiv.appendChild(price)
+
+		document.getElementById('products').appendChild(outerDiv)
+	});
 }
+
+
+setup()
